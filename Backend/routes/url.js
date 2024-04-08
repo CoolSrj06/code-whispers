84329754652865d7a573308acde240a11e6a37f4
CoolSrj06 <srijanmaurya6602@gmail.com>
2024-04-09 01:17:31 +0530

    Now images are also being displayed

--- Backend/routes/url.js
+++ Backend/routes/url.js
@@ -2,9 +2,31 @@ const express=require('express');
 const {handleHomePage}=require('./staticRouter');
 const router=express.Router();
 const app = express();
+const path = require('path'); 
+const fs=require('fs')
 
 
-app.use(express.static('../')); // Serve files from the 'public' directory
+app.use(express.static('../')); // Serve files from the directory
+app.use(express.static('../images/')); // Specifically for the 'images' folder
+
+router.get('/images/:imageName', (req, res) => {
+    const imageName = req.params.imageName;
+
+    // Build the correct path to the image file
+    const imagePath = path.join('..','images', imageName); 
+
+    // Check if the image file exists
+    fs.access(imagePath, fs.constants.F_OK, (err) => {
+        if (err) {
+            console.log(imagePath);
+            res.status(404).send('Image not found');
+        } else {
+            res.sendFile(imageName, { root: '../images' }); 
+        }
+    });
+});
+
+
 
 router.get('/styles.css', (req, res) => {
     res.sendFile('styles.css', { root: '../' }); // Adjust the path if needed
@@ -18,8 +40,8 @@ router.get('/developers.css', (req, res) => {
 router.get('/login.css', (req, res) => {
     res.sendFile('login.css', { root: '../' }); // Adjust the path if needed
 });
-router.get('/pakages.css', (req, res) => {
-    res.sendFile('pakages.css', { root: '../' }); // Adjust the path if needed
+router.get('/packages.css', (req, res) => {
+    res.sendFile('packages.css', { root: '../' }); // Adjust the path if needed
 });
 router.get('/portal.css', (req, res) => {
     res.sendFile('portal.css', { root: '../' }); // Adjust the path if needed
@@ -27,13 +49,6 @@ router.get('/portal.css', (req, res) => {
 router.get('/resources.css', (req, res) => {
     res.sendFile('resources.css', { root: '../' }); // Adjust the path if needed
 });
-
-// Image 
-router.get('/Ankit.png', (req, res) => {
-    res.sendFile('Ankit.png', { root: '../' }); // Adjust the path if needed
-});
-
-
 
 router.get('/', (req, res) => {
     res.sendFile('index.html', { root: '../' }); // Assuming 'index.html' is in the directory
