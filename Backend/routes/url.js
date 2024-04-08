ded4f197d172eadf6b9e0e58f40b4e7475c23042
CoolSrj06 <srijanmaurya6602@gmail.com>
2024-04-09 03:08:50 +0530

    Organised all the codes for better understanding

--- Backend/routes/url.js
+++ Backend/routes/url.js
@@ -2,67 +2,18 @@ const express=require('express');
 const {handleHomePage}=require('./staticRouter');
 const router=express.Router();
 const app = express();
-const path = require('path'); 
-const fs=require('fs')
+const{handleImagesRequest, handleCSSRequest, handleHTMLRequest} = require('../controller/url');
 
 
 app.use(express.static('../')); // Serve files from the directory
 app.use(express.static('../images/')); // Specifically for the 'images' folder
 
-router.get('/images/:imageName', (req, res) => {
-    const imageName = req.params.imageName;
-
-    // Build the correct path to the image file
-    const imagePath = path.join('..','images', imageName); 
-
-    // Check if the image file exists
-    fs.access(imagePath, fs.constants.F_OK, (err) => {
-        if (err) {
-            console.log(imagePath);
-            res.status(404).send('Image not found');
-        } else {
-            res.sendFile(imageName, { root: '../images' }); 
-        }
-    });
-});
-
-
-
-router.get('/styles.css', (req, res) => {
-    res.sendFile('styles.css', { root: '../' }); // Adjust the path if needed
-});
-router.get('/about.css', (req, res) => {
-    res.sendFile('about.css', { root: '../' }); // Adjust the path if needed
-});
-router.get('/developers.css', (req, res) => {
-    res.sendFile('developers.css', { root: '../' }); // Adjust the path if needed
-});
-router.get('/login.css', (req, res) => {
-    res.sendFile('login.css', { root: '../' }); // Adjust the path if needed
-});
-router.get('/packages.css', (req, res) => {
-    res.sendFile('packages.css', { root: '../' }); // Adjust the path if needed
-});
-router.get('/portal.css', (req, res) => {
-    res.sendFile('portal.css', { root: '../' }); // Adjust the path if needed
-});
-router.get('/resources.css', (req, res) => {
-    res.sendFile('resources.css', { root: '../' }); // Adjust the path if needed
-});
-
 router.get('/', (req, res) => {
-    res.sendFile('index.html', { root: '../' }); // Assuming 'index.html' is in the directory
+    res.sendFile('index.html', { root: '../' }); 
 });
-router.get('/resources.html', (req, res) => {
-    res.sendFile('resources.html', { root: '../' }); // Adjust the path if needed
-});
-router.get('/about.html', (req, res) => {
-    res.sendFile('about.html', { root: '../' }); // Adjust the path if needed
-});
-router.get('/packages.html', (req, res) => {
-    res.sendFile('packages.html', { root: '../' }); // Adjust the path if needed
-});
-router.get('/developers.html', (req, res) => {
-    res.sendFile('developers.html', { root: '../' }); // Adjust the path if needed
-});
+
+router.get('/:htmlFileName.html', handleHTMLRequest);
+router.get('/:cssFileName.css', handleCSSRequest);
+router.get('/images/:imageName',handleImagesRequest);
+
 module.exports=router;
